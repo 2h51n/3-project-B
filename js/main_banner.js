f964da74220fcb8e24aa044681275610feaaa019
LEEJUNGUEN <jungeun623@gmail.com>
2024-10-18 15:29:33 +0900

    배너 스와이퍼 효과 변경

--- js/main_banner.js
+++ js/main_banner.js
@@ -1,12 +1,15 @@
-document.addEventListener("DOMContentLoaded",function () {
-
-    const swiperBanner = new Swiper(".swiper-Banner", {
-      slidesPerView: 1, // 한 번에 보여질 슬라이드 수
-      loop: true, // loop : 무한으로 도는 것.
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false, // 상관없이 계속 autoplay.
-      },
-    });
-
+document.addEventListener("DOMContentLoaded", function () {
+  const swiperBanner = new Swiper(".swiper-Banner", {
+    effect: "fade",
+    // fadeEffect: {
+    //   crossFade: true, // 슬라이드 간 부드러운 전환
+    // },
+    slidesPerView: 1, // 한 번에 보여질 슬라이드 수
+    loop: true, // 무한 루프
+    autoplay: {
+      delay: 1500, // 2.5초마다 슬라이드 전환
+      disableOnInteraction: false, // 사용자가 슬라이드와 상호작용해도 자동 슬라이드 유지
+    },
+    
+  });
 });
