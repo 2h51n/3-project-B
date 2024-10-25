4fcf26a908409fa266bfcaecf8d637b46b2f5534
LEEJUNGUEN <jungeun623@gmail.com>
2024-10-25 11:06:29 +0900

    노티스배너/상세페이지/쿠폰

--- js/detail.js
+++ js/detail.js
@@ -1,45 +1,21 @@
-window.addEventListener("load", function () {
-  let detailImg = document.querySelectorAll(".detile-tab-button");
-  const cards = document.querySelectorAll(".card");
-  console.log(detailImg);
-  console.log(cards);
+window.addEventListener("load", () => {
+  const descriptionTab = document.getElementById("description-tab");
+  const brandTab = document.getElementById("brand-tab");
+  const descriptionContent = document.getElementById("detile-description");
+  const brandContent = document.getElementById("detile-brand");
 
-  //   로드시 활성화 메뉴랑 카드 index[0]
-  //
-  // detailImg[0].classList.add("active");
-  // cards[0].classList.add("active");
-
-  detailImg.forEach(function (detailImg, index) {
-    // console.log(menuItem);
-    detailImg.addEventListener("click", function () {
-      // //   클릭된 메뉴와 해당하는 카드를 비활성화
-      detailImg.forEach(function (item) {
-        // console.log(item);
-        item.classList.remove("active");
-      });
-      cards.forEach(function (card) {
-        console.log(card);
-        card.classList.remove("active");
-      });
-      //   클릭된 메뉴와 해당하는 카드를 활성화
-      detailImg[index].classList.add("active");
-      cards[index].classList.add("active");
-    });
+  // Add event listeners for tab clicks
+  descriptionTab.addEventListener("click", () => {
+    descriptionContent.style.display = "block";
+    brandContent.style.display = "none";
+    descriptionTab.classList.add("active");
+    brandTab.classList.remove("active");
   });
 
-  const hash = window.location.hash;
-  if (hash) {
-    const targetIndex = Array.from(detailImg).findIndex((item) => item.closest("li").id === hash.substring(1));
-    if (targetIndex !== -1) {
-      // 기존 활성화된 메뉴와 카드를 비활성화
-      detailImg.forEach((item) => item.classList.remove("active"));
-      cards.forEach((card) => card.classList.remove("active"));
-
-      // 해시로 지정된 메뉴와 카드 활성화
-      detailImg[targetIndex].classList.add("active");
-      cards[targetIndex].classList.add("active");
-    }
-  }
+  brandTab.addEventListener("click", () => {
+    descriptionContent.style.display = "none";
+    brandContent.style.display = "block";
+    brandTab.classList.add("active");
+    descriptionTab.classList.remove("active");
+  });
 });
-//누르면 다른거 다 보이기
-//다른거 누르면 클래스명으로 직관해서 작용하게
