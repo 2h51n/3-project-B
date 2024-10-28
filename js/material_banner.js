e365859714717614c9d53d572558e466cbbd262a
LEEJUNGUEN <jungeun623@gmail.com>
2024-10-28 13:14:27 +0900

    정렬

--- js/material_banner.js
+++ js/material_banner.js
@@ -5,7 +5,7 @@ const swiperBanner = new Swiper(".Banner", {
     slidesPerView: 1, // 한 번에 보여질 슬라이드 수
     loop: true, // 무한 루프
     autoplay: {
-      delay: 1000, // 2.5초마다 슬라이드 전환
+      delay: 800, // 2.5초마다 슬라이드 전환
       disableOnInteraction: false, // 사용자가 슬라이드와 상호작용해도 자동 슬라이드 유지
     },
   });
