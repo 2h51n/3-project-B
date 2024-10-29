a04bd32ef1134c65868c3aabccc93785dae2c9c2
LEEJUNGUEN <jungeun623@gmail.com>
2024-10-29 10:47:14 +0900

    아이콘 변경/정렬/디테일페이지 작업중

--- js/detail.js
+++ js/detail.js
@@ -3,6 +3,7 @@ window.addEventListener("load", () => {
   const brandTab = document.getElementById("brand-tab");
   const descriptionContent = document.getElementById("detile-description");
   const brandContent = document.getElementById("detile-brand");
+  const tabContainer = document.getElementById("detile-tab-container");
 
   // Add event listeners for tab clicks
   descriptionTab.addEventListener("click", () => {
@@ -13,6 +14,12 @@ window.addEventListener("load", () => {
   });
 
   brandTab.addEventListener("click", () => {
+    descriptionContent.style.display = "none";
+    brandContent.style.display = "block";
+    brandTab.classList.add("active");
+    descriptionTab.classList.remove("active");
+  });
+  tabContainer.addEventListener("click", () => {
     descriptionContent.style.display = "none";
     brandContent.style.display = "block";
     brandTab.classList.add("active");
